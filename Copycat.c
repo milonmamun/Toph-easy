ceedfc472b938d7e1179e1bf28e6e651b1fe5d46
almamun <almamun@mamun.com>
2025-05-28 19:23:23 +0600

    Commit- copycat-2 added.

--- Copycat.c
+++ Copycat.c
@@ -4,16 +4,8 @@
 int main(){
    
     int n;
-
-    printf("Enter an integer number(from -200000000 to 200000000):");
     scanf("%d",&n);
-
-    if(-200000000<=n && n<=200000000)
-    {
-        printf("\nYout entered number is: %d", n);
-        
-    }else
-        printf("Your entered number is not in range");
+    printf("%d", n);
     
     return 0;
 }
